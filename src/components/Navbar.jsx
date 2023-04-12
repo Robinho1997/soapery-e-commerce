fdc57cde125f2350593a3f2bcdd4ca5371d1c6ff
Robinho1997 <robin.kempen@gmx.de>
2023-04-12 13:52:46 +0200

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="first-ul">
+        <li>DE/EN</li>
+        <li>
+          <h1>SOAPERY</h1>
+        </li>
+        <li className="cart">
+          <span className="material-symbols-outlined">local_mall</span>
+        </li>
+      </ul>
+
+      <ul className="second-ul">
+        <li>SHOP</li>
+        <li>ABOUT</li>
+        <li>STORES</li>
+        <li>#SOAPERY</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
