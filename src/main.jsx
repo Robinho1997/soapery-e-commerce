3c41f75ad14d93aa2b2b6d10dee08930906760af
Robinho1997 <robin.kempen@gmx.de>
2023-04-16 23:03:56 +0200

    scroll up on every route  navigation

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ContextProvider } from "./Context";
 import App from "./App";
+import ScrollToTop from "./components/ScrollToTop";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <ContextProvider>
       <React.StrictMode>
+        <ScrollToTop />
         <App />
       </React.StrictMode>
     </ContextProvider>
